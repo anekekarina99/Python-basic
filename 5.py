ca956c43786607e371fa908ad1164a69e85f739b
Ane <42421075+anekekarina99@users.noreply.github.com>
2018-09-15 19:48:05 +0700

    Add files via upload
    Make asterisk from number 1 and 8

---
+++ 5.py
@@ -0,0 +1,83 @@
+#membuat angka 8
+num_list1 = [1]
+for y in num_list1:
+	print(y * '          *******')
+	
+num_list2 = [1,1,1,1,1]
+for y in num_list2:
+	print('          *     *' * y)
+	
+num_list3 = [1]
+for y in num_list3:
+	print(y * '          *******')
+	
+num_list4 = [1,1,1,1,1]
+for y in num_list4:
+	print('          *     *' * y)
+	
+num_list5 = [1]
+for y in num_list5:
+	print(y * '          *******')
+
+#membuat angka 1
+def asterisk (x ):
+	return x * 1
+
+print(asterisk(1))
+
+i = 2
+while True:
+	if i == 1:
+		break
+	i-=1
+	print(i*'  ***')
+
+# number_rows digunakan membuat angka 1
+number_rows = 5
+for l in range (number_rows):
+	print(   '    *')
+
+#number_column digunakan membuat angka 1
+i = 2
+while True:
+	if i ==	1:
+		break
+	i-=1
+	print(i*'  ***** ')
+
+#membuat angka 8
+print(asterisk(8))
+#while digunakan membuat colum 7
+i = 0
+while True:
+	if i == 7:
+		break
+	i+=7
+	print(i*        '*')
+
+# number_rows digunakan membuat angka 1
+number_colum = 5
+for l in range (number_colum):
+	print('*     *')
+
+#while digunakan membuat colum 8
+i = 0
+while True:
+	if i == 7:
+		break
+	i+=7
+	print(i*        '*')
+
+#menggunakan loop for membuat row 8
+num_list4 = [1,1,1,1,1]
+for y in num_list4:
+	print('*     *' * y)
+
+#while digunakan membuat colum 8
+i = 0
+while True:
+	if i == 7:
+		break
+	i+=7
+	print(i*        '*')
+
